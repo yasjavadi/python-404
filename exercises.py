4cd189939c97468ff5d87cbaecfaa66e87518db5
yasjavadi <zahra6161.javadi@gmail.com>
2025-11-13 11:23:54 -0800

    Add files via upload

---
+++ exercises.py
@@ -0,0 +1,17 @@
+#عددی را دریافت کرده و اعدد کوچکتر از آن را چاپ کنید 
+'''n=int(input("enter number:"))
+for i in range(n) :
+    print(i)'''
+
+ #اعداد زوج دو رقمی 
+'''for i in range(10,99):
+    if i%2!=0:
+     continue
+    print(i) '''
+# مضربهای عدد پنج کمتر از دویست
+for i in range(5,200) :
+ if i%5==0 :
+  print(i) 
+  
+
+     
