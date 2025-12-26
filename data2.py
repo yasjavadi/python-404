c23ffe31bc9bd0f7eee46398993f14c6aae7cf8b
yasjavadi <zahra6161.javadi@gmail.com>
2025-12-26 03:56:42 -0800

    data

--- data2.py
+++ data2.py
@@ -6,16 +6,10 @@ mdb = pymysql.connect(
     database='new_db'
 )
 con = mdb.cursor()
-kala_name=input('enetr name:')
-kala_price=input('enetr price:')
-kala_count=input('enetr count:')
-
 try:
-     sql = 'INSERT INTO `kala`(`name`, `price`, `count`) VALUES(%s,%s,%s)'
-     Myvar= (kala_name,kala_price,kala_count)
-     con.execute(sql, Myvar)
-     mdb.commit()
-     print('insert data')
+  Mysql = 'DELETE FROM `kala` WHERE  name ="tv"'
+  con.execute(Mysql)
+  mdb.commit()
+  print('deleted record')
 except:
-     print('can not insert')  
-     mdb.rollback()   
+  print("can not delet")
