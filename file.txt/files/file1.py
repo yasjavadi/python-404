4848e052537184d0def2bd58d2a5a70a72818d9d
yasjavadi <zahra6161.javadi@gmail.com>
2026-01-03 09:26:04 -0800

    update all

--- file.txt/files/file1.py
+++ file.txt/files/file1.py
@@ -1,5 +1,15 @@
+import csv
+
+'''with open("newfile1.csv","w") as csvfile:  
+ f= csv.DictWriter (csvfile,fieldnames=['name','age','class'])
+ f.writeheader()
+ f.writerow({'name':'zahra','age':22,'class':204})
+ f.writerow({'name':'reza','age':22,'class':205})'''
+
+with open ('person.csv','r') as csvfile:
+ f= csv.reader(csvfile)
+ for x in f :
+  print(x)
 
 
-with open("newfile1","w") as f:  
- f.write('helllllooooo')
 
